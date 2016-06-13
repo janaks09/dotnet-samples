2b50eae2c06ef358c55a1ec88252bb052ce567cd
Janak Shrestha <old_ink@hotmail.com>
2016-06-13 04:20:21 +0000

    added password hashing project

---
+++ PasswordHashing/PasswordHashing/CryptoService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace PasswordHashing
+{
+    public static class CryptoService
+    {
+        // 24 = 192 bits
+        private const int SaltByteSize = 24;
+        private const int HashByteSize = 24;
+        private const int HasingIterationsCount = 10101;
+
+
+        public static byte[] ComputeHash(string password, byte[] salt, int iterations = HasingIterationsCount, int hashByteSize = HashByteSize)
+        {
+            using (Rfc2898DeriveBytes hashGenerator = new Rfc2898DeriveBytes(password, salt))
+            {
+                hashGenerator.IterationCount = iterations;
+                return hashGenerator.GetBytes(hashByteSize);
+            }
+        }
+        
+        public static byte[] GenerateSalt(int saltByteSize = SaltByteSize)
+        {
+            using (RNGCryptoServiceProvider saltGenerator = new RNGCryptoServiceProvider())
+            {
+                byte[] salt = new byte[saltByteSize];
+                saltGenerator.GetBytes(salt);
+                return salt;
+            }
+        }
+
+        public static bool VerifyPassword(String password, byte[] passwordSalt, byte[] passwordHash)
+        {
+            byte[] computedHash = ComputeHash(password, passwordSalt);
+            return AreHashesEqual(computedHash, passwordHash);
+        }
+
+        //Length constant verification - prevents timing attack
+        private static bool AreHashesEqual(byte[] firstHash, byte[] secondHash)
+        {
+            int minHashLenght = firstHash.Length <= secondHash.Length ? firstHash.Length : secondHash.Length;
+            var xor = firstHash.Length ^ secondHash.Length;
+            for (int i = 0; i < minHashLenght; i++)
+                xor |= firstHash[i] ^ secondHash[i];
+            return 0 == xor;
+        }
+    }
+}
---
+++ PasswordHashing/PasswordHashing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PasswordHashing
+{
+    class Program
+    {
+        private const string password = "p@ssword!";
+        static void Main(string[] args)
+        {
+
+            var passwordSalt = CryptoService.GenerateSalt();
+            var passwordHash = CryptoService.ComputeHash(password, passwordSalt);
+
+            Console.WriteLine("PasswordResult:\n");
+            Console.WriteLine($"Plain Password: {password} \n");
+            Console.WriteLine($"PasswordHash: {Convert.ToBase64String(passwordHash)} \n");
+            Console.WriteLine($"PasswordSalt: {Convert.ToBase64String(passwordSalt)} \n");
+
+            Console.ReadLine();
+        }
+    }
+}
